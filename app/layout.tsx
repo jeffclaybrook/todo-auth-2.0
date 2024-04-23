53579a9ba8b983e83e44ab7a2c15c4d762db0225
Jeff Claybrook <jeffclaybrook@Jeffs-MBP.home>
2024-04-23 04:15:18 -0500

    Updated layout vercel url

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 const defaultUrl = process.env.VERCEL_URL
-  ? process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000"
 
 export const metadata: Metadata = {
